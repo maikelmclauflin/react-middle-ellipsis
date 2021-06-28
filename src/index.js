9463089b87acdcf046228788d7792f70e3171499
Michael McLaughlin <mmclaughlin@brave.com>
2021-06-28 11:10:53 -0500

    normalized package

--- src/index.js
+++ src/index.js
@@ -9,32 +9,21 @@ const getTextNode = node => {
 
 // read once
 const prepEllipse = node => {
-  const child =
-    node.childNodes[0]
-  const txtToEllipse =
-    node.querySelector(".ellipse-me") || child
+  const txtToEllipse = getTextNode(node)
 
   if (txtToEllipse === null) {
     return {}
   }
 
-  const attr =
-    txtToEllipse.getAttribute(dataOriginalKey)
+  const attr = txtToEllipse.getAttribute(dataOriginalKey)
 
   if (attr === null) {
     txtToEllipse.setAttribute(dataOriginalKey, getText(txtToEllipse))
   }
 
-  const checkSpan =
-    document.createElement("span")
+  const checkSpan = createMeasurementDevice(txtToEllipse)
 
-  // insert at end
-  checkSpan.style.fontSize = getComputedStyle(txtToEllipse).fontSize
-  checkSpan.style.position = "absolute"
-  checkSpan.style.visibility = "hidden"
-  checkSpan.style.pointerEvents = "none"
-  checkSpan.textContent = getText(txtToEllipse)
-
+  // insert at the end
   node.insertBefore(checkSpan, null)
 
   return {
@@ -43,8 +32,24 @@ const prepEllipse = node => {
   }
 }
 
-const ellipse = (checkSpan, txtToEllipse) => {
+const createMeasurementDevice = node => {
+  const checkSpan = document.createElement("span")
+
+  checkSpan.style.fontSize = getComputedStyle(node).fontSize
+  checkSpan.style.position = "absolute"
+  checkSpan.style.visibility = "hidden"
+  // required to not get in your mouse's way
+  checkSpan.style.pointerEvents = "none"
+  checkSpan.textContent = getText(node)
+  return checkSpan
+}
+
+const syncStyles = (checkSpan, txtToEllipse) => {
   checkSpan.style.fontSize = getComputedStyle(txtToEllipse).fontSize
+}
+
+const ellipse = (checkSpan, txtToEllipse, paddingPerc) => {
+  syncStyles(checkSpan, txtToEllipse)
 
   const fullWidth = checkSpan.offsetWidth
   const constrainedWidth = txtToEllipse.offsetWidth
@@ -52,13 +57,15 @@ const ellipse = (checkSpan, txtToEllipse) => {
 
   if (fullWidth > constrainedWidth) {
     const txtChars = str.length
-    const avgLetterSize = (fullWidth / txtChars) * 1.1
+    const paddingMultiplier = (100 + paddingPerc) / 100
+    const avgLetterSize = (fullWidth / txtChars) * paddingMultiplier // 10% padding by default
     const canFit = constrainedWidth / avgLetterSize
     const delEachSide = (txtChars - canFit) / 2
     const endLeft = Math.floor(txtChars / 2 - delEachSide)
     const startRight = Math.ceil(txtChars / 2 + delEachSide)
+    const ellipsed = str.substr(0, endLeft) + "..." + str.substr(startRight)
 
-    setText(txtToEllipse, str.substr(0, endLeft) + "..." + str.substr(startRight))
+    setText(txtToEllipse, ellipsed)
   } else {
     // set back to original
     setText(txtToEllipse, str)
@@ -76,6 +83,10 @@ const setText = (node, value) => {
 }
 
 const getText = node => {
+  const value = node.getAttribute(dataOriginalKey)
+  if (value != null) {
+    return value
+  }
   switch (node.tagName.toLowerCase()) {
     case "input":
       return node.value
@@ -84,46 +95,44 @@ const getText = node => {
   }
 }
 
-export default class Component extends React.Component {
+export default class ReactMiddleEllipsis extends React.Component {
   constructor(props) {
     super(props)
     this.ref = null
-    this.prepRelease = null
+    this.doEllipsis = null
     this.childRefs = {}
   }
   render() {
     const { props } = this
     const measuredParent = node => {
-      const { checkSpan } =
-        this.childRefs
+      const { checkSpan } = this.childRefs
 
       if (node === null) {
         return
-      } else if (this.prepRelease) {
-        window.removeEventListener("resize", this.prepRelease)
+      } else if (this.doEllipsis) {
+        window.removeEventListener("resize", this.doEllipsis)
         if (checkSpan && checkSpan.parentNode) {
           checkSpan.parentNode.removeChild(checkSpan)
         }
       }
       if (props.disabled) {
-        const txtToEllipse =
-          getTextNode(node)
-        const value =
-          txtToEllipse.getAttribute(dataOriginalKey)
+        const txtToEllipse = getTextNode(node)
+        const value = txtToEllipse.getAttribute(dataOriginalKey)
 
         if (value !== null) {
           setText(txtToEllipse, value)
         }
         return
       }
-      this.prepRelease = () => ellipse(
+      this.doEllipsis = () => ellipse(
         this.childRefs.checkSpan,
-        this.childRefs.txtToEllipse
+        this.childRefs.txtToEllipse,
+        props.paddingPerc == null ? 10 : props.paddingPerc,
       )
       this.ref = node
       this.childRefs = prepEllipse(node)
-      window.addEventListener("resize", this.prepRelease)
-      this.prepRelease()
+      window.addEventListener("resize", this.doEllipsis)
+      this.doEllipsis()
     }
 
     return (
@@ -133,7 +142,7 @@ export default class Component extends React.Component {
           wordBreak: "keep-all",
           overflowWrap: "normal",
           overflow: "hidden",
-          position: "relative",
+          position: "relative", // required to disallow overflow
           ...(props.width && { width: props.width })
         }}
       >
